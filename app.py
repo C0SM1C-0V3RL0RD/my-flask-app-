0c6e45c95e1f894cde13aaaf7ab61e014ab4e0b7
C0SM1C-0V3RL0RD <gaddark130@gmail.com>
2024-12-24 08:54:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Merhaba Flask!'
+    menu_items = [
+        {"name": "İskender Kebap", "price": 290, "image": "iskender.jpg"},
+        {"name": "Adana Kebap", "price": 260, "image": "adana.jpg"},
+        {"name": "Urfa Kebap", "price": 260, "image": "urfa.jpg"},
+        {"name": "Beyti Kebap", "price": 275, "image": "beyti.jpg"},
+        {"name": "Güveç", "price": 290, "image": "guvec.jpg"},
+        {"name": "Tandır", "price": 580, "image": "tandir.jpg"},
+    ]
+    return render_template("index.html", menu_items=menu_items)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True)
